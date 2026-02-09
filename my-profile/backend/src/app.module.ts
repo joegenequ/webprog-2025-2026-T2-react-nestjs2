f4eb51453e674e28b141ca04afe982df4f6adcd7
joegenequ <joegenequesada@gmail.com>
2026-02-09 11:50:03 +0000

    gemini commit 13

--- my-profile/backend/src/app.module.ts
+++ my-profile/backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GuestbookController } from './guestbook.controller';
+import { GuestbookService } from './guestbook.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, GuestbookController],
+  providers: [AppService, GuestbookService],
 })
 export class AppModule {}
